ca92e1a07c3f7364d1b5da8150e22df90bf54617
N-Tarin <karinuxxx1029@gmail.com>
2020-12-09 17:49:38 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+  
+  // トップに戻る
+  const pagetop = $('.page-top');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+
+
+
+
+
+
+});
+
