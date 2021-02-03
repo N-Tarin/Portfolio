41e4c6e3fe838ade959fdfaface8b289ee70986d
N-Tarin <karinuxxx1029@gmail.com>
2021-02-03 23:23:11 +0900

    add

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
 $(function() {
   
-  // トップに戻る
-  const pagetop = $('.page-top');
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      pagetop.fadeIn();
-    } else {
-      pagetop.fadeOut();
-    }
-  });
-  pagetop.click(function () {
-    $('body, html').animate({ scrollTop: 0 }, 500);
-    return false;
-  });
-
 });
 
