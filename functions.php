17ebaa51ed93232ba6701698bd68ea6caca7a91e
N-Tarin <karinuxxx1029@gmail.com>
2021-03-01 18:36:03 +0900

    changed

--- functions.php
+++ functions.php
@@ -4,9 +4,6 @@ function my_script_init() {
   // CSSの読み込み
   wp_enqueue_style('reset', get_template_directory_uri() . '/css/reset.css', array(), '1.0.0', 'all');
   wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');
-
-  // JSの読み込み 
-  wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array( 'jquery' ), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
 
